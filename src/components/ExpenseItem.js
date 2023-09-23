b33a430a078ef06b6156bf268cf68d776f03067c
amy <wlgpek90@naver.com>
2023-09-23 18:42:09 +0900

    props 사용하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,6 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
 
     return (
         <div className="expense-item">
