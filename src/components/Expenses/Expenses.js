3368405cdfcb8f59bdf993b3b20b34846208a9f9
amy <wlgpek90@naver.com>
2023-09-24 17:51:04 +0900

    chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2021');
@@ -22,15 +23,8 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {filteredExpenses.length === 0 ? <p>No expenses found.</p>: (filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key = {expense.id}
-                        title={expense.title }
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                )))}
-
+                <ExpensesChart expenses = {filteredExpenses}/>
+                <ExpensesList item={filteredExpenses}/>
             </Card>
         </div>
     );
